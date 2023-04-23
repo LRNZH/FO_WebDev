4bf2a3e033ecec6346c6316382e6173182468cd1
LRNZH <fullmentalfloss@gmail.com>
2023-04-23 14:58:41 +0100

    Prod build

--- part3/phonebook/3.9-/backend/index.js
+++ part3/phonebook/3.9-/backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.static("build"));
 
 app.use(express.json());
 
@@ -118,7 +123,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
